d11b58404685ed2a339c155b3b5d23c228871d6b
Jimmy King <jimmy.king@meltmedia.com>
2013-02-19 16:28:36 -0700

    Most of the functionality is done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 $(function() {
+  $.getJSON("https://api.twitter.com/1/statuses/user_timeline.json?screen_name=KendraWorsnup&count=1&include_rts=1&callback=?", function(data) {
 
+  var d = new Date();
+  var tweetId = data[0].id;
+  var tweetCreated = data[0].created_at.split(' ');
+  var tweetTime = tweetCreated[3];
+  var currentTime = d.getUTCHours() + ":" + d.getUTCMinutes() + ":" + d.getUTCSeconds();
+    
+  var getMinutes = function(time) {
+    time = time.split(':');
+    var minutes = 0;
+    minutes += parseInt(time[0]) * 60;
+    minutes += parseInt(time[1]);
+    return minutes;
+  }
+
+  var tweet = data[0].text.split(' ');
+
+  if (tweet.indexOf('#KJZZ') != -1 &&
+      tweetCreated[2] == d.getDate() &&
+      (getMinutes(currentTime) - getMinutes(tweetTime)) <= 15) {
+    $('.no').hide();
+    $('.yes').show();
+  }
+  });
 });
