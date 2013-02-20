bd91c0744fd31d4ba472f36ee84f03155040dd63
Jimmy King <jimmy.king@meltmedia.com>
2013-02-20 16:46:20 -0700

    Made it a little more efficient

--- js/main.js
+++ js/main.js
@@ -1,32 +1,29 @@
 $(function() {
   $.getJSON("https://api.twitter.com/1/statuses/user_timeline.json?screen_name=KendraWorsnup&count=1&include_rts=1&callback=?", function(data) {
-
-  var d = new Date();
-  var tweetId = data[0].id;
-  var tweetCreated = data[0].created_at.split(' ');
-  var tweetTime = tweetCreated[3];
-  var currentTime = d.getUTCHours() + ":" + d.getUTCMinutes() + ":" + d.getUTCSeconds();
+    var tweet = data[0].text.split(' '),
+        tweetCreated = data[0].created_at.split(' '),
+        tweetTime = data[0].created_at.split(' ')[3],
+        d = new Date(),
+        currentTime = d.getUTCHours() + ":" + d.getUTCMinutes() + ":" + d.getUTCSeconds();
+      
+    var jimmyMinutes = function(time) {
+      time = time.split(':');
+      var minutes = 0;
+      minutes += parseInt(time[0],10) * 60;
+      minutes += parseInt(time[1],10);
+      return minutes;
+    }
     
-  var jimmyMinutes = function(time) {
-    time = time.split(':');
-    var minutes = 0;
-    minutes += parseInt(time[0],10) * 60;
-    minutes += parseInt(time[1],10);
-    return minutes;
-  }
-
-  var tweet = data[0].text.split(' ');
-
-  for (i = 0; i < tweet.length; i++) {
-    tweet[i] = tweet[i].toUpperCase();
-  }
-
-  if (tweet.indexOf('#KJZZ') != -1 &&
-      tweetCreated[2] == d.getUTCDate() &&
-      (jimmyMinutes(currentTime) - jimmyMinutes(tweetTime)) <= 15) {
-    $('.no').hide();
-    $('.yes').show();
-    $('audio').attr('autoplay','autoplay');
+    for (i = 0; i < tweet.length; i++) {
+      tweet[i] = tweet[i].toUpperCase();
+    }
+  
+    if (tweet.indexOf('#KJZZ') != -1 &&
+        tweetCreated[2] == d.getUTCDate() &&
+        (jimmyMinutes(currentTime) - jimmyMinutes(tweetTime)) <= 15) {
+      $('.no').hide();
+      $('.yes').show();
+      $('audio').attr('autoplay','autoplay');
     }
   });
 });
