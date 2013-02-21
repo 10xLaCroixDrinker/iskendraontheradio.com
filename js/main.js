ae2c2322cf9b8e6b8c7432249eee56604aab487e
Jimmy King <jimmy.king@meltmedia.com>
2013-02-20 17:02:30 -0700

    Implemented FitText.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(function() {
+
+  $('h2').fitText(0.2);
+
   $.getJSON("https://api.twitter.com/1/statuses/user_timeline.json?screen_name=KendraWorsnup&count=1&include_rts=1&callback=?", function(data) {
     var tweet = data[0].text.split(' '),
         tweetCreated = data[0].created_at.split(' '),
